afbb3e08eead460774e7e6f82bca7f144f6d6dc9
Sebbern96 <herrbismo@gmail.com>
2022-05-03 11:43:10 +0200

    frontend logic for ChatApplication

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDom from "react-dom";
 
 function Login({ onLogin }) {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("test");
   function handleSubmit(event) {
     event.preventDefault();
     onLogin(username);
@@ -25,18 +25,49 @@ function Login({ onLogin }) {
   );
 }
 
+function ChatMessage({ chat: { author, message } }) {
+  return (
+    <div>
+      <strong>{author}</strong>
+      {message}
+    </div>
+  );
+}
+
 function ChatApplication({ username }) {
+  const [chatLog, setChatLog] = useState([
+    {
+      author: "Sebastian",
+      message: "Skjer gutta,",
+    },
+    {
+      author: "Vegard",
+      message: "Du er den perfekte mann Sebastian",
+    },
+    {
+      author: "Ramstad",
+      message: "Poker eller gutta?",
+    },
+  ]);
+  const [message, setMessage] = useState("");
+
   function handleNewMessage(event) {
     event.preventDefault();
+    setChatLog([...chatLog, { author: username, message }]);
+    setMessage("");
   }
 
   return (
     <div className={"application"}>
       <header>Chat Application {username}</header>
-      <main>Here is the main content</main>
+      <main>
+        {chatLog.map((chat, index) => (
+          <ChatMessage key={index} chat={chat} />
+        ))}
+      </main>
       <footer>
         <form onSubmit={handleNewMessage}>
-          <input />
+          <input value={message} onChange={(e) => setMessage(e.target.value)} />
           <button>Submit</button>
         </form>
       </footer>
