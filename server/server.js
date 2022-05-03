72d773551322997ebd3f7ede5d455f31b539cbcb
Sebbern96 <herrbismo@gmail.com>
2022-05-03 13:43:04 +0200

    Message recivied from server

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ app.use(express.static("../client/dist"));
 
 const wsServer = new WebSocketServer({ noServer: true });
 wsServer.on("connect", (socket) => {
-  console.log("ws connected");
+  socket.send("Pls send");
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`http://localhost:${server.address().port}`);
   server.on("upgrade", (req, socket, head) => {
-    wsServer.handleUpgrade(req, socket, head, () => {});
-
-    wsServer.emit("connect", socket, req);
+    wsServer.handleUpgrade(req, socket, head, (socket) => {
+      wsServer.emit("connect", socket, req);
+    });
   });
 });
